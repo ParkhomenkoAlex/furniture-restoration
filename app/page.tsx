80319fa0b8163d79e6adee76d30327107316dbf5
Alexander Parkhomenko <alexforai89@gmail.com>
2026-08-07 16:02:54 +0300

    added new project text to main

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
     return (
         <main>
-            <h1>DEVELOPMENT TEST</h1>
+            <h1>added new project DEVELOPMENT TEST</h1>
             <p>Furniture restoration project</p>
         </main>
     );
