b13d8cf2f4316e83b55955fecf92479011a3730c
Alexander Parkhomenko <alexforai89@gmail.com>
2026-08-11 13:37:23 +0300

    add about section

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import { Header } from "./components/landing/Header";
 import { Hero } from "./components/landing/Hero";
 import { Services } from "./components/landing/Services";
 import {HowItWorks} from "@/app/components/landing/HowItWorks";
+import {About} from "@/app/components/landing/About";
 
 export default function HomePage() {
     return (
@@ -12,6 +13,7 @@ export default function HomePage() {
                 <Hero />
                 <Services />
                 <HowItWorks />
+                <About />
             </main>
         </>
     );
